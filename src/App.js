d7ef26d2eac8cf81ffadd888125ccc2ec77fbb0f
Bartlomiej Figatowski <bartek_19_83@hotmail.com>
2020-09-27 17:00:50 -0400

    server and client pagination

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect, Fragment } from "react";
+import axios from "axios";
 
-function App() {
+import { MDBContainer, MDBListGroup, MDBListGroupItem, MDBBtn } from "mdbreact";
+
+const App = () => {
+  const [userCredentials, setUserCredentials] = useState([]);
+  const [pagination, setPagination] = useState([]);
+  const [skip, setSkip] = useState(3);
+  const [dataLength, setdataLength] = useState(null);
+  const handleSubmit = async (e) => {
+    try {
+      const limit = 3;
+      const { data } = await axios.get(
+        `http://localhost:8080/pagination?skip=${skip}&limit=${limit}`
+      );
+      setdataLength(data.length);
+      setPagination([...pagination, ...data]);
+      setSkip(skip + limit);
+      return data;
+    } catch (error) {
+      //message, config, code, request, response response.data
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const { data } = await axios.get("http://localhost:8080/");
+        setUserCredentials(data);
+        return data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <MDBContainer>
+      <MDBListGroup style={{ width: "22rem" }}>
+        {userCredentials
+          ? Object.values(userCredentials).map((items) => {
+              return (
+                <MDBListGroupItem key={items._id}>
+                  {items.company}
+                </MDBListGroupItem>
+              );
+            })
+          : null}
 
-export default App;
+        {pagination.map((items) => {
+          return (
+            <MDBListGroupItem key={items._id}>{items.company}</MDBListGroupItem>
+          );
+        })}
+      </MDBListGroup>
+
+      <Fragment>
+        {dataLength !== 0 ? (
+          <MDBBtn color="danger" onClick={() => handleSubmit()}>
+            Load more...
+          </MDBBtn>
+        ) : null}
+      </Fragment>
+    </MDBContainer>
+  );
+};
+
+export { App };
